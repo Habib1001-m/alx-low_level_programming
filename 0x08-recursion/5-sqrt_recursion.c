08ae0ade0cac70cf0c3084a41636b3c9c255e6bc
Mohamed Habib <mohamedhabib49.mh@gmail.com>
2023-12-01 14:32:41 -0800

    fix for task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * check - Checks for the square root.
- * @a: Integer.
- * @b: Integer.
+ * check - checks for the square root
+ * @a:int
+ * @b:int
  *
- * Return: Integer.
+ * Return: int
  */
 int check(int a, int b)
 {
@@ -17,17 +17,13 @@ int check(int a, int b)
 }
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: Integer to find the square root of.
- *
- * Return: Natural square root or -1.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	else if (n == 0)
+	if (n == 0)
 		return (0);
-	else
-		return (check(1, n));
-}}
+	return (check(1, n));
+}
